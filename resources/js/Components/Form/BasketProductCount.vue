5576b95d9cb90298acdae4b245329dda5407b652
Radoslav Tomas <radoslav.tomas@gmail.com>
2022-10-30 15:17:40 +0000

    improved product count in basket

---
+++ resources/js/Components/Form/BasketProductCount.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="flex justify-start items-center">
+        <div class="h-full flex justify-start items-center">
+            <div class="border border-r-0 border-gray-300 rounded-l w-7 h-7 flex justify-center items-center">
+                <TrashIcon v-if="count === 1" class="h-4 w-4 text-red-500 hover:text-red-600 cursor-pointer" />
+                <MinusIcon v-else @click="decrementProductCount" class="h-4 w-4 text-red-500 hover:text-red-600 cursor-pointer" />
+            </div>
+            <label class="block">
+                <input :value="count" type="tel" class="
+                                    text-sm
+                                    text-center
+                                    h-7
+                                    w-12
+                                    block
+                                    w-full
+                                    border-gray-300
+                                    shadow-sm
+                                    focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+            </label>
+            <div class="border border-l-0 border-gray-300 rounded-r w-7 h-7 flex justify-center items-center">
+                <PlusIcon @click="incrementProductCount" class="h-4 w-4 text-teal-600 hover:text-teal-700 cursor-pointer" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+
+import { TrashIcon, PlusIcon, MinusIcon } from '@heroicons/vue/24/outline';
+
+const count = ref(1)
+
+const incrementProductCount = () => count.value++;
+const decrementProductCount = () => count.value--;
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/Pages/Basket.vue
+++ resources/js/Pages/Basket.vue
@@ -1,9 +1,10 @@
 <script setup>
 import { Head, Link } from '@inertiajs/inertia-vue3';
 import MainLayout from '../Layouts/MainLayout.vue'
+import BasketProductCount from '../Components/Form/BasketProductCount.vue'
 
 import FormInput from '../Components/Form/FormInput.vue';
-import { TrashIcon } from '@heroicons/vue/24/outline';
+import { TrashIcon, PlusIcon } from '@heroicons/vue/24/outline';
 
 defineProps({
     category: String,
@@ -18,79 +19,58 @@ defineProps({
     <main-layout>
         <section class="my-4 max-w-xl mx-auto">
             <div class="grid grid-cols-12 gap-4 py-4 border-b border-gray-300">
-                <div class="col-span-3 sm:col-span-2">
-                    <img class="w-20 h-auto shadow border border-gray-200 rounded" src="/storage/sites/default/files/Nanichodnica_1.png" alt="book.title">
+                <div class="col-span-4 sm:col-span-2">
+                    <img class="w-32 sm:w-20 h-auto shadow border border-gray-200 rounded" src="/storage/sites/default/files/Nanichodnica_1.png" alt="book.title">
                 </div>
 
-                <div class="col-span-6 sm:col-span-8 flex flex-col justify-between items-stretch">
-                    <div>
-                        <h4 class="text-base sm:text-lg uppercase">Nanichodnica</h4>
-                        <h6 class="text-sm italic">Jana Juranova</h6>
+                <div class="col-span-8 flex flex-col justify-between items-stretch">
+                    <div class="mb-2">
+                        <h4 class="text-sm sm:text-base uppercase font-bold">Nanichodnica</h4>
+                        <h6 class="text-sm italic mb-1">Jana Juranova</h6>
+                        <h3 class="sm:hidden font-bold">8,93€</h3>
                     </div>
-                    <div class="flex justify-start items-center">
-                        <div class="w-16 h-full flex justify-start items-center">
-                            <FormInput type="number"/>
-                        </div>
-
-                        <span class="ml-3">
-                            <TrashIcon class="h-4 w-4 text-red-500 hover:text-red-600 cursor-pointer" />
-                        </span>
-                    </div>
+                    <BasketProductCount />
                 </div>
 
-                <div class="col-span-3 sm:col-span-2 text-sm sm:text-base text-right font-bold">
+                <div class="hidden sm:block col-span-2 text-right font-bold">
                     8,93€
                 </div>
             </div>
 
             <div class="grid grid-cols-12 gap-4 py-4 border-b border-gray-300">
-                <div class="col-span-3 sm:col-span-2">
-                    <img class="w-20 h-auto shadow border border-gray-200 rounded" src="/storage/sites/default/files/JEDNOROZCE VACSIE.jpg" alt="book.title">
+                <div class="col-span-4 sm:col-span-2">
+                    <img class="w-32 sm:w-20 h-auto shadow border border-gray-200 rounded" src="/storage/sites/default/files/JEDNOROZCE VACSIE.jpg" alt="book.title">
                 </div>
 
-                <div class="col-span-6 sm:col-span-8 flex flex-col justify-between items-stretch">
-                    <div>
-                        <h4 class="text-base sm:text-lg uppercase">Jednorozce</h4>
-                        <h6 class="text-sm italic">Jana Juranova</h6>
+                <div class="col-span-8 flex flex-col justify-between items-stretch">
+                    <div class="mb-2">
+                        <h4 class="text-sm sm:text-base uppercase font-bold">Jednorozce</h4>
+                        <h6 class="text-sm italic mb-1">Jana Juranova</h6>
+                        <h3 class="sm:hidden font-bold">8,93€</h3>
                     </div>
-                    <div class="flex justify-start items-center">
-                        <div class="w-16 h-full flex justify-start items-center">
-                            <FormInput type="number"/>
-                        </div>
-
-                        <span class="ml-3">
-                            <TrashIcon class="h-4 w-4 text-red-500 hover:text-red-600 cursor-pointer" />
-                        </span>
-                    </div>
+                    <BasketProductCount />
                 </div>
 
-                <div class="col-span-3 sm:col-span-2 text-sm sm:text-base text-right font-bold">
+                <div class="hidden sm:block col-span-2 text-right font-bold">
                     8,93€
                 </div>
             </div>
 
             <div class="grid grid-cols-12 gap-4 py-4 border-b border-gray-300">
-                <div class="col-span-3 sm:col-span-2">
-                    <img class="w-20 h-auto shadow border border-gray-200 rounded" src="/storage/sites/default/files/Ako odvravat novembru 1989_0.png" alt="book.title">
+                <div class="col-span-4 sm:col-span-2">
+                    <img class="w-32 sm:w-20 h-auto shadow border border-gray-200 rounded" src="/storage/sites/default/files/Ako odvravat novembru 1989_0.png" alt="book.title">
                 </div>
 
-                <div class="col-span-6 sm:col-span-8 flex flex-col justify-between items-stretch">
-                    <div>
-                        <h4 class="text-base sm:text-lg uppercase">Ako odvravat Novembru a mozno este dlhsi nazov, nez by sme cakali</h4>
-                        <h6 class="text-sm italic">Jana Juranova</h6>
+                <div class="col-span-8 flex flex-col justify-between items-stretch">
+                    <div class="mb-2">
+                        <h4 class="text-sm sm:text-base uppercase font-bold">Ako odvravat Novembru a mozno este dlhsi nazov, nez by sme cakali</h4>
+                        <h6 class="text-sm italic mb-1">Jana Juranova</h6>
+                        <h3 class="sm:hidden font-bold">8,93€</h3>
                     </div>
-                    <div class="flex justify-start items-center">
-                        <div class="w-16 h-full flex justify-start items-center">
-                            <FormInput type="number"/>
-                        </div>
-
-                        <span class="ml-3">
-                            <TrashIcon class="h-4 w-4 text-red-500 hover:text-red-600 cursor-pointer" />
-                        </span>
-                    </div>
+                    <BasketProductCount />
                 </div>
 
-                <div class="col-span-3 sm:col-span-2 text-sm sm:text-base text-right font-bold">
+                <div class="hidden sm:block col-span-2 text-right font-bold">
                     8,93€
                 </div>
             </div>
