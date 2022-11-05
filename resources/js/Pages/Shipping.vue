0e538270cfcc5f099ee5c7dedf7888e2d45ac6ad
Radoslav Tomas <radoslav.tomas@gmail.com>
2022-11-05 17:15:24 +0000

    validation for shipping form wip

--- resources/js/Pages/Shipping.vue
+++ resources/js/Pages/Shipping.vue
@@ -5,7 +5,7 @@ import {Link} from "@inertiajs/inertia-vue3";
 import {ArrowRightCircleIcon} from '@heroicons/vue/24/outline';
 import { useForm } from '@inertiajs/inertia-vue3'
 import useVuelidate from '@vuelidate/core'
-import { required, email, minLength } from '@vuelidate/validators'
+import { required, email, requiredIf } from '@vuelidate/validators'
 import {reactive, ref, computed} from "vue";
 
 import FormInput from '../Components/Form/FormInput.vue';
@@ -15,44 +15,63 @@ import FormTextarea from '../Components/Form/FormTextarea.vue';
 import Card from '../Components/Card.vue';
 
 const options = [
-    {value: '', description: '- Prosím vyberte si -'},
-    {value: 'SK', description: 'Slovensko'},
-    {value: 'CZ', description: 'Česká republika'},
-    {value: 'DE', description: 'Nemecko'},
-    {value: 'PL', description: 'Poľsko'},
-    {value: 'GB', description: 'Veľká Británia'},
-    {value: 'AT', description: 'Rakúsko'},
-    {value: 'US', description: 'Spojené štáty'},
-    {value: 'CA', description: 'Kanada'},
+    {value: '703', description: 'Slovensko'},
+    {value: '203', description: 'Česká republika'},
+    {value: '276', description: 'Nemecko'},
+    {value: '616', description: 'Poľsko'},
+    {value: '826', description: 'Veľká Británia'},
+    {value: '40', description: 'Rakúsko'},
+    {value: '840', description: 'Spojené štáty'},
+    {value: '124', description: 'Kanada'},
 ]
 
 const form = reactive({
-    email: '',
-    phone: '',
-    state: 'SK'
+    primary_email: '',
+    delivery_phone: '',
+    delivery_first_name: '',
+    delivery_last_name: '',
+    delivery_company: '',
+    delivery_street1: '',
+    delivery_street2: '',
+    delivery_city: '',
+    delivery_postal_code: '',
+    delivery_country: '703',
+    billing_first_name: '',
+    billing_last_name: '',
+    billing_company: '',
+    billing_street1: '',
+    billing_street2: '',
+    billing_city: '',
+    billing_postal_code: '',
+    billing_country: '703',
 })
 
 const showBillingPanel = ref(false);
 
 const rules = computed(() => {
     return {
-        email: {
-            required, email
-        },
-        phone: {
-            required
-        },
-        state: {
-            required
-        }
+        primary_email: { required, email },
+        delivery_phone: { required },
+        delivery_first_name: { required },
+        delivery_last_name: { required },
+        delivery_company: { required },
+        delivery_street1: { required },
+        delivery_city: { required },
+        delivery_postal_code: { required },
+        delivery_country: { required },
+        billing_first_name: { requiredIf: requiredIf(showBillingPanel) },
+        billing_last_name: { requiredIf: requiredIf(showBillingPanel) },
+        billing_company: { requiredIf: requiredIf(showBillingPanel) },
+        billing_street1: { requiredIf: requiredIf(showBillingPanel) },
+        billing_city: { requiredIf: requiredIf(showBillingPanel) },
+        billing_postal_code: { requiredIf: requiredIf(showBillingPanel) },
+        billing_country: { requiredIf: requiredIf(showBillingPanel) },
     }
 })
 
 const v$ = useVuelidate(rules, form);
 
 const handleForm = async () => {
-    console.log('new')
-    console.log(v$.value.$email)
     const result = await v$.value.$validate();
 
     if(!result) {
@@ -100,38 +119,96 @@ defineProps({
             <form @submit.prevent="handleForm">
                 <Card title="Informácie o zákazníkovi/zákazníčke">
                     <FormInput
-                        v-model.trim="form.email"
+                        v-model.trim="form.primary_email"
                         title="Email *" type="text"
                         placeholder="Tu Vám zašleme potvrdenie o objednávke"
-                        :errors="v$.email.$errors.length ? v$.email.$errors[0] : null" />
+                        :errors="v$.primary_email.$errors.length ? v$.primary_email.$errors[0] : null" />
 
                     <FormInput
-                        v-model.trim="form.phone"
+                        v-model.trim="form.delivery_phone"
                         title="Telefónne číslo"
-                        :errors="v$.phone.$errors.length ? v$.phone.$errors[0] : null"
+                        :errors="v$.delivery_phone.$errors.length ? v$.delivery_phone.$errors[0] : null"
                     />
                 </Card>
 
                 <Card title="Adresa doručenia">
-                    <FormInput title="Meno *" />
-                    <FormInput title="Priezvisko *" />
-                    <FormInput title="Spoločnosť" />
-                    <FormInput title="Ulica *" />
-                    <FormInput title="Mesto *" />
-                    <FormInput title="PSČ *" />
-                    <FormSelect v-model="form.state" title="Štát" :options="options" />
+                    <FormInput
+                        v-model.trim="form.delivery_first_name"
+                        title="Meno *" type="text"
+                        :errors="v$.delivery_first_name.$errors.length ? v$.delivery_first_name.$errors[0] : null" />
+
+                    <FormInput
+                        v-model.trim="form.delivery_last_name"
+                        title="Priezvisko *" type="text"
+                        :errors="v$.delivery_last_name.$errors.length ? v$.delivery_last_name.$errors[0] : null" />
+
+                    <FormInput
+                        v-model.trim="form.delivery_company"
+                        title="Spoločnosť *" type="text"
+                        :errors="v$.delivery_company.$errors.length ? v$.delivery_company.$errors[0] : null" />
+
+                    <FormInput
+                        v-model.trim="form.delivery_street1"
+                        title="Ulica *" type="text"
+                        :errors="v$.delivery_street1.$errors.length ? v$.delivery_street1.$errors[0] : null" />
+
+                    <FormInput
+                        v-model.trim="form.delivery_city"
+                        title="Mesto *" type="text"
+                        :errors="v$.delivery_city.$errors.length ? v$.delivery_city.$errors[0] : null" />
+
+                    <FormInput
+                        v-model.trim="form.delivery_postal_code"
+                        title="PSČ *" type="text"
+                        :errors="v$.delivery_postal_code.$errors.length ? v$.delivery_postal_code.$errors[0] : null" />
+
+                    <FormSelect
+                        v-model="form.delivery_country"
+                        title="Štát"
+                        :errors="v$.delivery_country.$errors.length ? v$.delivery_country.$errors[0] : null"
+                        :options="options" />
+
                     <br>
+
                     <FormCheckbox v-model="showBillingPanel" title="Moja fakturačná adresa je iná ako dodacia adresa." />
                 </Card>
 
                 <Card v-if="showBillingPanel" title="Fakturačné údaje">
-                    <FormInput title="Meno *" />
-                    <FormInput title="Priezvisko *" />
-                    <FormInput title="Spoločnosť" />
-                    <FormInput title="Ulica *" />
-                    <FormInput title="Mesto *" />
-                    <FormInput title="PSČ *" />
-                    <FormSelect title="Štát" :options="options" />
+                    <FormInput
+                        v-model.trim="form.billing_first_name"
+                        title="Meno *" type="text"
+                        :errors="v$.billing_first_name.$errors.length ? v$.billing_first_name.$errors[0] : null" />
+
+                    <FormInput
+                        v-model.trim="form.billing_last_name"
+                        title="Priezvisko *" type="text"
+                        :errors="v$.billing_last_name.$errors.length ? v$.billing_last_name.$errors[0] : null" />
+
+                    <FormInput
+                        v-model.trim="form.billing_company"
+                        title="Spoločnosť *" type="text"
+                        :errors="v$.billing_company.$errors.length ? v$.billing_company.$errors[0] : null" />
+
+                    <FormInput
+                        v-model.trim="form.billing_street1"
+                        title="Ulica *" type="text"
+                        :errors="v$.billing_street1.$errors.length ? v$.billing_street1.$errors[0] : null" />
+
+                    <FormInput
+                        v-model.trim="form.billing_city"
+                        title="Mesto *" type="text"
+                        :errors="v$.billing_city.$errors.length ? v$.billing_city.$errors[0] : null" />
+
+                    <FormInput
+                        v-model.trim="form.billing_postal_code"
+                        title="PSČ *" type="text"
+                        :errors="v$.billing_postal_code.$errors.length ? v$.billing_postal_code.$errors[0] : null" />
+
+                    <FormSelect
+                        v-model="form.billing_country"
+                        title="Štát"
+                        :errors="v$.billing_country.$errors.length ? v$.billing_country.$errors[0] : null"
+                        :options="options" />
                     <br>
                 </Card>
 
