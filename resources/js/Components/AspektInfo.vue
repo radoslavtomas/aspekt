68be61067999165f225b8932103a364d9a4a8d15
Radoslav Tomas <radoslav.tomas@gmail.com>
2023-01-04 20:40:57 +0000

    changed order of info

--- resources/js/Components/AspektInfo.vue
+++ resources/js/Components/AspektInfo.vue
@@ -15,20 +15,6 @@
             </div>
 
             <div class="p-6">
-<!--                @TODO: select link for library-->
-                <Link :href="route('library', 'kedy-a-kde')" class="flex  items-center ml-2 text-lg leading-7 hover:text-red-400">
-                    <BuildingLibraryIcon class="h-5 w-5 mr-2" />
-                    Knižnica ASPEKTU
-                </Link>
-
-                <div class="">
-                    <div class="mt-2 text-sm">
-                        Knižnica ASPEKTU  sa špecializuje sa na literatúru s feministickou a rodovou problematikou, ponúka široký výber odbornej literatúry a beletrie v rôznych jazykoch (okrem slovenčiny a češtiny najmä v angličtine, nemčine a poľštine). Uchováva plagáty, brožúry, periodiká, audiovizuálne dokumenty a rôzne iné materiály.
-                    </div>
-                </div>
-            </div>
-
-            <div class="p-6">
                 <Link :href="route('aspektin', 'vsetko')" class="flex items-center ml-2 text-lg leading-7 hover:text-red-400">
                     <NewspaperIcon class="h-5 w-5 mr-2" />
                     ASPEKTin
@@ -37,6 +23,20 @@
                 <div class="">
                     <div class="mt-2 text-sm">
                         V roku 1993 vyšlo prvé číslo feministického kultúrneho časopisu Aspekt, ktorý bol ako slovensko-české médium prvým feministickým časopisom v bývalom Československu a dnes pokračuje ako webzin ASPEKTin. Naša organizácia sa ako nositeľka množstva vzdelávacích a publikačných projektov od svojich začiatkov zameriava na rodovú senzibilizáciu spoločnosti.
+                    </div>
+                </div>
+            </div>
+
+            <div class="p-6">
+                <!--                @TODO: select link for library-->
+                <Link :href="route('library', 'kedy-a-kde')" class="flex  items-center ml-2 text-lg leading-7 hover:text-red-400">
+                    <BuildingLibraryIcon class="h-5 w-5 mr-2" />
+                    Knižnica ASPEKTU
+                </Link>
+
+                <div class="">
+                    <div class="mt-2 text-sm">
+                        Knižnica ASPEKTU  sa špecializuje sa na literatúru s feministickou a rodovou problematikou, ponúka široký výber odbornej literatúry a beletrie v rôznych jazykoch (okrem slovenčiny a češtiny najmä v angličtine, nemčine a poľštine). Uchováva plagáty, brožúry, periodiká, audiovizuálne dokumenty a rôzne iné materiály.
                     </div>
                 </div>
             </div>
