74d4c046738021fafe60de302bd2ffca2ffa7e29
Radoslav Tomas <radoslav.tomas@gmail.com>
2024-10-22 07:11:56 +0100

    fixed typo in variable

--- resources/views/components/email-header.blade.php
+++ resources/views/components/email-header.blade.php
@@ -1,6 +1,6 @@
 {{--@formatter:off--}}
 <div style="box-sizing:border-box;width:100%;text-align: center;padding: 16px 10px;background-color:#f6f6f6">
 <a href="https://www.aspekt.sk">
-<img style="height:3rem;width:auto;border:none;display: inline-block;" src="{{ config('app.app_url') . '/assets/img/aspekt_logo.png' }}" alt="Aspekt logo">
+<img style="height:3rem;width:auto;border:none;display: inline-block;" src="{{ config('app.url') . '/assets/img/aspekt_logo.png' }}" alt="Aspekt logo">
 </a>
 </div>
